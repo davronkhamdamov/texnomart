fdf1b3986dd4f033198b2ca449feb6c8a82eb953
Davron <109721370+davronkhamdamov@users.noreply.github.com>
2023-05-14 20:30:33 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,15 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyDtlEnT9Q0KHRvbedwdt_LoiAXDiqPzyzk',
-  authDomain: 'fir-79c34.firebaseapp.com',
-  projectId: 'fir-79c34',
-  storageBucket: 'fir-79c34.appspot.com',
-  messagingSenderId: '520439091757',
-  appId: '1:520439091757:web:d3e4841b517951f2c55ea4',
-  measurementId: 'G-DM9R521SQ0',
+  apiKey: "AIzaSyAmlcxiP2PckhNgHJvPJc6t5lToI111h9E",
+  authDomain: "test-phone-d9b16.firebaseapp.com",
+  projectId: "test-phone-d9b16",
+  storageBucket: "test-phone-d9b16.appspot.com",
+  messagingSenderId: "366636623479",
+  appId: "1:366636623479:web:18f7329bd46525ec614872",
+  measurementId: "G-897P0YCKKC"
 };
 
 const beckend = firebase.initializeApp(firebaseConfig);
